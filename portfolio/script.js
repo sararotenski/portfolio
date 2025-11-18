54f83fcbee17615b0177c7c754d71d1ce8640f99
Sara Rotenski Pereira <sararotenskipereira@gmail.com>
2025-11-18 19:16:27 -0300

    terceiro trimestre

--- portfolio/script.js
+++ portfolio/script.js
@@ -29,6 +29,10 @@ function bancoDeDados() {
     window.location.href="banco-de-dados.html";
 }
 
+function devSistemas() {
+    window.location.href="dev-de-sistemas.html";
+}
+
 //carrega a página extras
 function extras() {
     window.location.href="extras.html";
@@ -41,9 +45,10 @@ function scrollSobreMim() {
     });
 }
 
-//Criando o modal do primeiro trimestre de matemática
+//Criando os modais de cada trimestre de matemática
 const modalPrimeiroTriMat = document.getElementById("modal-primeiro-mat");
 const modalSegundoTriMat = document.getElementById("modal-segundo-mat");
+const modalTerceiroTriMat = document.getElementById('modal-terceiro-mat')
 
 function primeiroTrimestreMatematica() {
     modalPrimeiroTriMat.style.display = "flex";
@@ -53,9 +58,14 @@ function segundoTrimestreMatematica() {
     modalSegundoTriMat.style.display = "flex";
 }
 
+function terceiroTrimestreMatematica() {
+    modalTerceiroTriMat.style.display = 'flex';
+}
+
 function fecharMat() {
     modalPrimeiroTriMat.style.display = "none";
     modalSegundoTriMat.style.display = "none";
+    modalTerceiroTriMat.style.display = "none";
 }
 
 window.addEventListener("click", function(event) {
@@ -70,6 +80,13 @@ window.addEventListener("click", function(event) {
     }
 });
 
+window.addEventListener("click", function(event) {
+    if (event.target === modalTerceiroTriMat) {
+        fecharMat();
+    }
+});
+
+//modal da atividade de matemática
 const modalPasseMat = document.getElementById("modal-passe-mat-background");
 
 function abrirModalMatPasse() {
@@ -86,106 +103,135 @@ window.addEventListener("click", function(event) {
     }
 });
 
-//Criando o modal do primeiro trimestre de humanas
+//Criando os modais de cada trimestre de humanas
 const modalPrimeiroTriHumanas = document.getElementById("modal-primeiro-humanas");
+const modalSegundoTriHumanas = document.getElementById("modal-segundo-humanas");
+const modalTerceiroTriHumanas = document.getElementById('modal-terceiro-humanas');
 
 function primeiroTrimestreHumanas() {
     modalPrimeiroTriHumanas.style.display = "flex";
 }
 
-function fecharPrimeiroTriHumanas() {
-    modalPrimeiroTriHumanas.style.display = "none";
-}
-
-window.addEventListener("click", function(event) {
-    if (event.target === modalPrimeiroTriHumanas) {
-        fecharPrimeiroTriHumanas();
-    }
-});
-
-const modalSegundoTriHumanas = document.getElementById("modal-segundo-humanas");
-
 function segundoTrimestreHumanas() {
     modalSegundoTriHumanas.style.display = "flex";
 }
 
-function fecharSegundoTriHumanas() {
+function terceiroTrimestreHumanas() {
+    modalTerceiroTriHumanas.style.display = 'flex';
+}
+
+function fecharHumanas() {
+    modalPrimeiroTriHumanas.style.display = "none";
     modalSegundoTriHumanas.style.display = "none";
+    modalTerceiroTriHumanas.style.display = "none";
 }
 
 window.addEventListener("click", function(event) {
-    if (event.target === modalSegundoTriHumanas) {
-        fecharSegundoTriHumanas();
+    if (event.target === modalPrimeiroTriHumanas) {
+        fecharHumanas();
     }
 });
 
-//Criando o modal do primeiro trimestre de modelagem de sistemas
+window.addEventListener("click", function(event) {
+    if (event.target === modalSegundoTriHumanas) {
+        fecharHumanas();
+    }
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target === modalTerceiroTriHumanas) {
+        fecharHumanas();
+    }
+});
+
+//Criando os modais de cada trimestre de modelagem de sistemas
 const modalPrimeiroTriModelagem = document.getElementById("modal-primeiro-modelagem");
+const modalSegundoTriModelagem = document.getElementById("modal-segundo-modelagem");
 
 function primeiroTrimestreModelagem() {
     modalPrimeiroTriModelagem.style.display = "flex";
 }
 
-function fecharPrimeiroTriModelagem() {
-    modalPrimeiroTriModelagem.style.display = "none";
-}
-
-window.addEventListener("click", function(event) {
-    if (event.target === modalPrimeiroTriModelagem) {
-        fecharPrimeiroTriModelagem();
-    }
-});
-
-const modalSegundoTriModelagem = document.getElementById("modal-segundo-modelagem");
-
 function segundoTrimestreModelagem() {
     modalSegundoTriModelagem.style.display = "flex";
 }
 
-function fecharSegundoTriModelagem() {
+function fecharModelagem() {
+    modalPrimeiroTriModelagem.style.display = "none";
     modalSegundoTriModelagem.style.display = "none";
 }
 
 window.addEventListener("click", function(event) {
-    if (event.target === modalSegundoTriModelagem) {
-        fecharSegundoTriModelagem();
+    if (event.target === modalPrimeiroTriModelagem) {
+        fecharModelagem();
     }
 });
 
-//Criando o modal do primeiro trimestre de linguagens
+window.addEventListener("click", function(event) {
+    if (event.target === modalSegundoTriModelagem) {
+        fecharModelagem();
+    }
+});
+
+//Criando os modais de cada trimestre de linguagens
 const modalPrimeiroTriLinguagens = document.getElementById("modal-primeiro-linguagens");
+const modalSegundoTriLinguagens = document.getElementById('modal-segundo-linguagens');
+const modalTerceiroTriLinguagens = document.getElementById('modal-terceiro-linguagens');
 
 function primeiroTrimestreLinguagens() {
     modalPrimeiroTriLinguagens.style.display = "flex";
 }
 
-function fecharPrimeiroTriLinguagens() {
-    modalPrimeiroTriLinguagens.style.display = "none";
-}
-
-window.addEventListener("click", function(event) {
-    if (event.target === modalPrimeiroTriLinguagens) {
-        fecharPrimeiroTriLinguagens();
-    }
-});
-
-//Criando o modal do segundo trimestre de linguagens
-const modalSegundoTriLinguagens = document.getElementById('modal-segundo-linguagens');
-
 function segundoTrimestreLinguagens() {
     modalSegundoTriLinguagens.style.display = "flex";
 }
 
-function fecharSegundoTriLinguagens() {
+function terceiroTrimestreLinguagens() {
+    modalTerceiroTriLinguagens.style.display = 'flex';
+}
+
+function fecharLinguagens() {
+    modalPrimeiroTriLinguagens.style.display = "none";
     modalSegundoTriLinguagens.style.display = "none";
+    modalTerceiroTriLinguagens.style.display = 'none';
 }
 
 window.addEventListener("click", function(event) {
-    if (event.target === modalSegundoTriLinguagens) {
-        fecharSegundoTriLinguagens();
+    if (event.target === modalPrimeiroTriLinguagens) {
+        fecharLinguagens();
     }
 });
 
+window.addEventListener("click", function(event) {
+    if (event.target === modalSegundoTriLinguagens) {
+        fecharLinguagens();
+    }
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target === modalTerceiroTriLinguagens) {
+        fecharLinguagens();
+    }
+});
+
+//Criando o modal da nota da redação (primeiro trimestre)
+const modalNotaRedacao = document.getElementById("modal-redacao-background");
+
+function abrirModalNotaRedacao() {
+    modalNotaRedacao.style.display = "flex";
+}
+
+function fecharModalNotaRedacao() {
+    modalNotaRedacao.style.display = "none";
+}
+
+window.addEventListener("click", function(event) {
+    if (event.target === modalNotaRedacao) {
+        fecharModalNotaRedacao();
+    }
+});
+
+//Criando o modal da nota da redação (segundo trimestre)
 const modalNotaRedacaoDois = document.getElementById("modal-redacao2-background");
 
 function abrirModalNotaRedacaoDois() {
@@ -202,20 +248,20 @@ window.addEventListener("click", function(event) {
     }
 });
 
-//Criando o modal da nota da redação
-const modalNotaRedacao = document.getElementById("modal-redacao-background");
+//Criando o modal da nota da redação (terceiro trimestre)
+const modalNotaRedacaoTres = document.getElementById("modal-redacao3-background");
 
-function abrirModalNotaRedacao() {
-    modalNotaRedacao.style.display = "flex";
+function abrirModalNotaRedacaoTres() {
+    modalNotaRedacaoTres.style.display = "flex";
 }
 
-function fecharModalNotaRedacao() {
-    modalNotaRedacao.style.display = "none";
+function fecharModalNotaRedacaoTres() {
+    modalNotaRedacaoTres.style.display = "none";
 }
 
 window.addEventListener("click", function(event) {
-    if (event.target === modalNotaRedacao) {
-        fecharModalNotaRedacao();
+    if (event.target === modalNotaRedacaoTres) {
+        fecharModalNotaRedacaoTres();
     }
 });
 
@@ -236,36 +282,44 @@ window.addEventListener("click", function(event) {
     }
 });
 
-//Criando o modal do primeiro trimestre de natureza
+//Criando os modais de cada trimestre de natureza
 const modalPrimeiroTriNatureza = document.getElementById("modal-primeiro-natureza");
+const modalSegundoTriNatureza = document.getElementById('modal-segundo-natureza');
+const modalTerceiroTriNatureza = document.getElementById('modal-terceiro-natureza');
 
 function primeiroTrimestreNatureza() {
     modalPrimeiroTriNatureza.style.display = "flex";
 }
 
-function fecharPrimeiroTriNatureza() {
-    modalPrimeiroTriNatureza.style.display = "none";
-}
-
-window.addEventListener("click", function(event) {
-    if (event.target === modalPrimeiroTriNatureza) {
-        fecharPrimeiroTriNatureza();
-    }
-});
-
-const modalSegundoTriNatureza = document.getElementById('modal-segundo-natureza');
-
 function segundoTrimestreNatureza() {
     modalSegundoTriNatureza.style.display = "flex";
 }
 
-function fecharSegundoTriNatureza() {
+function terceiroTrimestreNatureza() {
+    modalTerceiroTriNatureza.style.display = 'flex';
+}
+
+function fecharNatureza() {
+    modalPrimeiroTriNatureza.style.display = "none";
     modalSegundoTriNatureza.style.display = "none";
+    modalTerceiroTriNatureza.style.display = 'none';
 }
 
 window.addEventListener("click", function(event) {
+    if (event.target === modalPrimeiroTriNatureza) {
+        fecharNatureza();
+    }
+});
+
+window.addEventListener("click", function(event) {
     if (event.target === modalSegundoTriNatureza) {
-        fecharSegundoTriNatureza();
+        fecharNatureza();
+    }
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target === modalTerceiroTriNatureza) {
+        fecharNatureza();
     }
 });
 
@@ -286,6 +340,23 @@ window.addEventListener("click", function(event) {
     }
 });
 
+//Criando o modal do mapa mental de sistema imunológico e vacinas
+const modalMapaImunologico = document.getElementById("modal-imunologico-background");
+
+function abrirModalImunologico() {
+    modalMapaImunologico.style.display = "flex";
+}
+
+function fecharModalImunologico() {
+    modalMapaImunologico.style.display = "none";
+}
+
+window.addEventListener("click", function(event) {
+    if (event.target === modalMapaImunologico) {
+        fecharModalImunologico();
+    }
+});
+
 //Criando o modal de banco de dados
 const modalSegundoTriBanco = document.getElementById('modal-segundo-banco');
 
@@ -303,21 +374,20 @@ window.addEventListener("click", function(event) {
     }
 });
 
-//Criando o modal do mapa mental de sistema imunológico e vacinas
+//Criando o modal de desenvolvimento de sistemas
+const modalTerceiroTriDev = document.getElementById('modal-terceiro-dev');
 
-const modalMapaImunologico = document.getElementById("modal-imunologico-background");
-
-function abrirModalImunologico() {
-    modalMapaImunologico.style.display = "flex";
+function terceiroTrimestreDev() {
+    modalTerceiroTriDev.style.display = 'flex';
 }
 
-function fecharModalImunologico() {
-    modalMapaImunologico.style.display = "none";
+function fecharDev() {
+    modalTerceiroTriDev.style.display = 'none';
 }
 
 window.addEventListener("click", function(event) {
-    if (event.target === modalMapaImunologico) {
-        fecharModalImunologico();
+    if (event.target === modalTerceiroTriDev) {
+        fecharDev();
     }
 });
 
@@ -338,6 +408,7 @@ window.addEventListener("click", function(event) {
     }
 });
 
+//Criando o modal da foto da Iniciação Científica
 const modalIniciacao = document.getElementById("modal-iniciacao-background");
 
 function abrirModaliniciacao() {
